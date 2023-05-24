35c0bae5cf5470183972fceef2679f767b265ba6
Tymur Baksheiev <bak.ttt@gmail.com>
2023-05-25 02:20:23 +0300

    Minor code cleanup.

--- RectExercise.Application.Implementation/Services/RectanglesService.cs
+++ RectExercise.Application.Implementation/Services/RectanglesService.cs
@@ -15,15 +15,6 @@ namespace RectExercise.Application.Implementation.Services
 
         public async Task<IEnumerable<PointMatchDto>> GetRectanglesByMatchingPointsAsync(IReadOnlyList<PointDto> points)
         {
-            /*return Task.FromResult<IEnumerable<PointMatchDto>>(
-                points
-                    .Take(Random.Shared.Next(points.Count))
-                    .Select(x => new PointMatchDto(x, new[] {
-                        new RectangleDto(
-                            new PointDto(1, 2),
-                            new PointDto(4, 5))
-                    }))
-                    .ToList());*/
             var rectangles = await _rectanglesRepository.GetRectanglesByMatchingPointsAsync(points);
             // TODO: match points & rectangles
 
