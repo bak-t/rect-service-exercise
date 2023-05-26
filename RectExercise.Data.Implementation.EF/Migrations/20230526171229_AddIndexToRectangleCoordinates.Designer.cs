0717fede5c0c91d579bb61c7423cbe8892c0fec2
Tymur Baksheiev <bak.ttt@gmail.com>
2023-05-26 20:21:06 +0300

    Created index for Rectangles table.

--- RectExercise.Data.Implementation.EF/Migrations/20230526171229_AddIndexToRectangleCoordinates.Designer.cs
+++ RectExercise.Data.Implementation.EF/Migrations/20230526171229_AddIndexToRectangleCoordinates.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RectExercise.Data.Implementation.EF.Database;
 
@@ -11,9 +12,11 @@ using RectExercise.Data.Implementation.EF.Database;
 namespace RectExercise.Data.Implementation.EF.Migrations
 {
     [DbContext(typeof(RectDbContext))]
-    partial class RectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230526171229_AddIndexToRectangleCoordinates")]
+    partial class AddIndexToRectangleCoordinates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RectExercise.Data.Implementation.EF/Migrations/20230526171229_AddIndexToRectangleCoordinates.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RectExercise.Data.Implementation.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexToRectangleCoordinates : Migration
+    {
+        const string IndexName = "IX_Rectangles_Coordinates";
+        const string TableName = "Rectangles";
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: IndexName,
+                table: TableName,
+                columns: new[] {
+                    "Left",
+                    "Top",
+                    "Right",
+                    "Bottom",
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: IndexName, table: TableName);
+        }
+    }
+}
